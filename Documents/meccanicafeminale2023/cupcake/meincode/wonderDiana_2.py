d76f101cf4cb23d82b2b3fb9e63b43ef71e33ebf
DianaBarth <diana.barth@studium.fernuni-hagen.de>
2023-02-25 06:18:20 +0100

    vor erstem test

--- Documents/meccanicafeminale2023/cupcake/meincode/wonderDiana_2.py
+++ Documents/meccanicafeminale2023/cupcake/meincode/wonderDiana_2.py
@@ -20,20 +20,28 @@ class Position(object):
 
 class Umgebung(object):
     
-    def __init__(self, bezeichung,startBegrenzung, endBegrenzung):
+    def __init__(self, bezeichung, endBegrenzung, wechselVerb):
         self.bezeichung =bezeichung
         self.geschwindigkeiten = {}
         self.verbtypen = {}
-        self.startBegrenzung = startBegrenzung
         self.endBegrenzung = endBegrenzung
-      
-    def gebeBezeichnung(self):
-        return self.bezeichung
-        
+        self.offset = 0
+        self.naechsteUmgebung = None
+        self.wechselVerb = wechselVerb
+
+    def setzeNaechsteUmgebung(self,naechsteUmgebung)    
+        self.naechsteUmgebung = naechsteUmgebung
+    
+    def setzeOffset(self, offset)
+        self.offset = offset
+    
     def setzeGeschwindigkeit(self, verb, wert, typ)
         self.geschwindigkeiten[verb] = wert          
         self.verbtypen[verb] = typ
 
+    def gebeBezeichnung(self):
+        return self.bezeichung
+        
     def gebeGeschwindigkeit(self,verb):
         return self.geschwindigkeiten[verb]
   
@@ -43,34 +51,37 @@ class Umgebung(object):
     def gebeVerben(self):
         return self.geschwindigkeiten.keys()
 
+    def gebeWechselverb(self):
+        retrun self.wechselVerb
+
     def testeEndBegrenzung(self, userposition):
         if (userposition.gebeX() == endbegrenzung.gebeX()  and userposition.gebeY() == endbegrenzung.gebeY() ):
             return "genau nord-west oben" ## ecke
-        elif (userposition.gebeX() == endbegrenzung.gebeX() - offset and userposition.gebeY() == endbegrenzung.gebeY() - offset):
+        elif (userposition.gebeX() == endbegrenzung.gebeX() - self.offset and userposition.gebeY() == endbegrenzung.gebeY() - self.offset):
             return "offset nord-west oben" ## ecke
         elif (userposition.gebeX() == endbegrenzung.gebeX() and userposition.gebeY() == - endbegrenzung.gebeY()):
             return "genau süd-west" ## ecke
-        elif (userposition.gebeX() == endbegrenzung.gebeX() - offset and userposition.gebeY() == - endbegrenzung.gebeY() + offset):
+        elif (userposition.gebeX() == endbegrenzung.gebeX() - self.offset and userposition.gebeY() == - endbegrenzung.gebeY() + self.offset):
             return "offset süd-west" ## ecke
         elif (userposition.gebeX() == - endbegrenzung.gebeX() and userposition.gebeY() == - endbegrenzung.gebeY()):
             return "genau süd-ost" ## ecke
-        elif (userposition.gebeX() == - endbegrenzung.gebeX() + offset and userposition.gebeY() == - (endbegrenzung.gebeY()) + offset):
+        elif (userposition.gebeX() == - endbegrenzung.gebeX() + self.offset and userposition.gebeY() == - (endbegrenzung.gebeY()) + self.offset):
             return "offset süd-ost" ## ecke
         elif userposition.gebeX() == endbegrenzung.gebeX():
             return "genau ost"
-        elif userposition.gebeX() == endbegrenzung.gebeX() - offset:
+        elif userposition.gebeX() == endbegrenzung.gebeX() - self.offset:
             return "offset ost"
         elif userposition.gebeX() == - endbegrenzung.gebeX():
             return "genau west"
-        elif userposition.gebeX() == - endbegrenzung.gebeX() + offset:
+        elif userposition.gebeX() == - endbegrenzung.gebeX() + self.offset:
             return "offset west"
         elif userposition.gebeY() == endbegrenzung.gebeY():
             return "genau nord"
-        elif userposition.gebeY() == endbegrenzung.gebeY() - offset:
+        elif userposition.gebeY() == endbegrenzung.gebeY() - self.offset:
             return "offset nord"
         elif userposition.gebeY() == - endbegrenzung.gebeY():
             return "genau süd"
-        elif userposition.gebeY() == - endbegrenzung.gebeY() + offset:
+        elif userposition.gebeY() == - endbegrenzung.gebeY() + self.offset:
             return "offset süd"     
         else:
             return "kleineres" 
@@ -95,11 +106,23 @@ class Umgebung(object):
 
         for verb in  self.umgebung.gebeVerben()            
             if verb in self.eingabe:
-                anwendung = self.umgebung.gebeVerbtyp(verb)
-                if anwendung == "Fläche":
-                    self.bewegeFläche(verb)
-                elif anwendung =="Ebene":
-                    self.bewegeEbene(verb)
+                
+               grenzTest = self.umgebung.testeEndBegrenzung(self.position) 
+               
+               if grenzTest == "kleineres":
+
+                    anwendung = self.umgebung.gebeVerbtyp(verb)
+                    if anwendung == "Fläche":
+                        self.bewegeFläche(verb)
+                    elif anwendung =="Ebene":
+                        self.bewegeEbene(verb)
+
+                elif "genau" in grenzTest :
+                        self.text = "Du stehst genau an einer Grenze."
+                
+                elif "offset" in grenzTest:
+                    self.text = "du stehst kurz vor einer Grenze"
+
 
     #Himmelsrichtungen
 
@@ -144,7 +167,7 @@ class Umgebung(object):
         self.text = "Du " + verb + " nach " + self.richtungstext
 
     def bewegeEbene(self, verb)
-    
+
         geschwindigkeit = self.umgebung.gebeGeschwindigkeit(verb)
 
         if "nord" in self.eingabe:
@@ -174,25 +197,35 @@ def hauptProgramm
     grenzPositionWaldTreppe = Position (50,50,10)
     grenzPositiondoktormutter = Position(55,55,10)
 
-    startUmgebung = Umgebung("start",Position(0,0,0),grenzPositionStartWasser) 
+    startUmgebung = Umgebung("start",grenzPositionStartWasser,  "springe") 
     startUmgebung.setzeGeschwindigkeit("gehst",2, "Fläche")
     startUmgebung.setzeGeschwindigkeit("rennst",4, "Fläche")
+    startUmgebung.setzeOffset(2)
   
-    wasserUmgebung = Umgebung("wasser",grenzPositionStartWasser,grenzPositionWasserWald) 
+    wasserUmgebung = Umgebung("wasser",grenzPositionWasserWald, "springe") 
     wassertUmgebung.setzeGeschwindigkeit("schwimmst",4, "Ebene")
 
-    waldUmgebung = Umgebung("start",grenzPositionWasserWald,grenzPositionWasserWald) 
+    waldUmgebung = Umgebung("wald",grenzPositionWaldTreppe, "steige") 
     waldUmgebung.setzeGeschwindigkeit("gehst", 1, "Fläche")
     waldUmgebung.setzeGeschwindigkeit("rennst",2 , "Fläche")
+    waldUmgebung.setzeOffset(1)
 
-    treppeUmgegung = Umgenung("treppe, "grenzPositionWasserWald, grenzPositionWaldTreppe)
+    treppeUmgegung = Umgenung("treppe", grenzPositiondoktormutter, "spreche")
     treppeUmgegung.setzeGeschwindigkeit("steigst",4, "Ebene")
+
+    startUmgebung.setzeNaechsteUmgebung(wasserUmgebung)
+    wasserUmgebung.setzeNaechsteUmgebung(waldUmgebung)
+    waldUmgebung.setzeNaechsteUmgebung(treppeUmgegung)
 
     bewegung = Bewegung(Position(0,0,0), startUmgebung)
 
-    letzeUserself.eingabe ="starte"
-    doktormutterPosition = startPosition 
-
+    while True:
+        
+        usereingabe = input("> ").casefold()
+        bewegung.bewege(usereingabe)
+        bewegung.druckeText()
+        bewegung.druckePosition()
+        
     Inventar  = []
     
     while True:
